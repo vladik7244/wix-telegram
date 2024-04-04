1a293b53ef68296be035ebf7ffba8f9a7ac8fa70
vladyslavh <vladyslavh@wix.com>
2024-04-04 22:12:30 +0200

    protocol

--- wix-telegram.js
+++ wix-telegram.js
@@ -7,4 +7,58 @@ window.addEventListener('message', (event) => {
   if (event.data === 'hideButton') {
     Telegram.WebApp.MainButton.hide()
   }
+  const msg = tryParseMsg(event.data);
+  if (!msg) {
+    return;
+  }
+  processMessage(msg);
 });
+
+function processMessage(msg) {
+  if (msg.action === 'read') {
+    return processReadRequest(msg)
+  }
+  if (msg.action === 'action') {
+    return processAction(msg)
+  }
+}
+
+function processReadRequest(msg) {
+  const result = path(Telegram.WebApp, msg.path);
+  const resultStr = JSON.stringify(result);
+  localStorage.setItem(msg.address, resultStr);
+}
+
+function processAction(msg) {
+  const method = path(Telegram.WebApp, msg.path);
+  const result = method(...msg.args);
+  if (msg.resultAddress) {
+    localStorage.setItem(msg.resultAddress, JSON.stringify(result));
+  }
+}
+
+function tryParseMsg(data) {
+  if (typeof data !== 'string') {
+    return;
+  }
+  try {
+    const messageObj = JSON.parse(data);
+    if (messageObj.type === '@com') {
+      return messageObj;
+    }
+  } catch (e) {
+    return;
+  }
+}
+
+function path(obj, path) {
+  return pathArr(obj, path.split('.'));
+}
+
+function pathArr(obj, arr) {
+  if (arr.length === 0) {
+    return obj;
+  }
+  const [first, ...rest] = arr;
+  return pathArr(obj[first], rest);
+}
